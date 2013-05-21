89cc86f3e18d82245c906e9ac2d18079248742fb
Denis Averin <ddenex@gmail.com>
2013-05-21 16:08:00 +0700

    session removed from tests

--- ical/tests.py
+++ ical/tests.py
@@ -11,27 +11,30 @@ from django.core.urlresolvers import reverse
 class MyTests(TestCase):
 
     url_list = [
-        ('/about/', 200),
-        ('/ical/', 200),
-        ('/ical/url/', 200),
-        ('/ical/file/', 200),
-        ('/ical/get_csv/', 302),
-        ('/ical/show_table/', 302),
-        ('/ical/download_csv/', 302),
-        ('/ical/error/', 200),
+        ('/about/', ''),
+        ('/ical/', ''),
+        ('/ical/url/', ''),
+        ('/ical/file/', ''),
+        ('/ical/get_csv/', '/ical/url/'),
+        ('/ical/show_table/', '/ical/'),
+        ('/ical/download_csv/', '/ical/'),
+        ('/ical/error/', ''),
     ]
 
     def test_urls(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-        for url, expected_status_code in self.url_list:
+        for url, redirect in self.url_list:
             response = self.client.get(url)
-            try:
-                self.assertEqual(response.status_code, expected_status_code)
-            except AssertionError as e:
-                print "URL:", url
-                raise e
+            if redirect:
+                try:
+                    self.assertRedirects(response, redirect)
+                except Exception, e:
+                    print "URL:", url, redirect
+                    raise e
+            else:
+                self.assertEqual(response.status_code, 200)
 
     def test_url_form(self):
         url = "http://www.google.com/calendar/ical/350imrtqvd076a106dbdfofagk%40group.calendar.google.com/public/basic.ics"
@@ -48,13 +51,8 @@ class MyTests(TestCase):
 
         # POST valid URL
         response = self.client.post(reverse('ical_post_url'), {'url': url})
-        # Session vars must be set up
-        self.assertEqual(self.client.session['ical_src_url'], url)
-        self.assertEqual(self.client.session['ical_src_file'], file_name)
         # Must redirect to 'ical_show_table'
         self.assertRedirects(response, reverse('ical_get_csv'), target_status_code=302)
-        # 'ical_table' must exists in session
-        self.assertTrue(self.client.session['ical_table'])
         # Must show table
         response = self.client.get(reverse('ical_show_table'))
         self.assertEqual(response.status_code, 200)

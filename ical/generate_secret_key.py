e4a58baa7afb397789f2aa932d07f5b337f30e4a
Denis Averin <ddenex@gmail.com>
2013-06-16 15:34:55 +0700

    generate_settings_key changed to works without django

--- ical/generate_secret_key.py
+++ ical/generate_secret_key.py
@@ -1,12 +1,18 @@
 from __future__ import with_statement
 
-from django.utils.crypto import get_random_string
+import random
+random = random.SystemRandom()
 
 
 def generate_secret_key(filename):
-    print filename
     chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-    secret = get_random_string(50, chars)
-    with open(filename, 'w') as f:
-        f.write('SECRET_KEY="%s"' % secret)
-    return True
+    length = 50
+    secret = ''.join([random.choice(chars) for i in range(length)])
+    if filename:
+        with open(filename, 'w') as f:
+            f.write('SECRET_KEY="%s"' % secret)
+    return secret
+
+
+if __name__ == '__main__':
+    print generate_secret_key(None)
